88b125f62ffc74b0641862c34ea9adc9db280677
elevencode <archcores@gmail.com>
2023-06-01 13:04:55 +0500

    fix: refactor app env

--- data/lib/utils/app_env.dart
+++ data/lib/utils/app_env.dart
@@ -3,13 +3,13 @@ import 'dart:io';
 abstract class AppEnv {
   const AppEnv._();
 
-  static final String secretKey = Platform.environment['SECRET_KEY'] ?? 'SECRET_KEY';
-  static final String port = Platform.environment['PORT'] ?? '6200';
+  static final String secretKey = Platform.environment['SECRET_KEY'] ?? '';
+  static final String port = Platform.environment['PORT'] ?? '';
 
   /// DB.
-  static final String dbUsername = Platform.environment['DB_USERNAME'] ?? 'admin';
-  static final String dbPassword = Platform.environment['DB_PASSWORD'] ?? 'root';
-  static final String dbHost = Platform.environment['DB_HOST'] ?? 'localhost';
-  static final int? dbPort = int.tryParse(Platform.environment['DB_PORT'] ?? '6201');
-  static final String dpDatabaseName = Platform.environment['DB_NAME'] ?? 'postgres';
+  static final String dbUsername = Platform.environment['DB_USERNAME'] ?? '';
+  static final String dbPassword = Platform.environment['DB_PASSWORD'] ?? '';
+  static final String dbHost = Platform.environment['DB_HOST'] ?? '';
+  static final int? dbPort = int.tryParse(Platform.environment['DB_PORT'] ?? '');
+  static final String dpDatabaseName = Platform.environment['DB_NAME'] ?? '';
 }
